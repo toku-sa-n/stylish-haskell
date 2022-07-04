6ce819c4d49e63c0498ab2b7c55f22d4da257bd0
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-04 09:23:47 +0900

    Apply the formatter

--- lib/Language/Haskell/Stylish.hs
+++ lib/Language/Haskell/Stylish.hs
@@ -1,29 +1,29 @@
 {-# LANGUAGE LambdaCase #-}
+
 --------------------------------------------------------------------------------
 module Language.Haskell.Stylish
-    ( -- * Run
-      runSteps
+      -- * Run
+  ( runSteps
       -- * Steps
-    , simpleAlign
-    , imports
-    , languagePragmas
-    , tabs
-    , trailingWhitespace
-    , unicodeSyntax
+  , simpleAlign
+  , imports
+  , languagePragmas
+  , tabs
+  , trailingWhitespace
+  , unicodeSyntax
       -- ** Helpers
-    , findHaskellFiles
-    , stepName
+  , findHaskellFiles
+  , stepName
       -- * Config
-    , module Language.Haskell.Stylish.Config
+  , module Language.Haskell.Stylish.Config
       -- * Misc
-    , module Language.Haskell.Stylish.Verbose
-    , version
-    , format
-    , ConfigPath(..)
-    , Lines
-    , Step
-    ) where
-
+  , module Language.Haskell.Stylish.Verbose
+  , version
+  , format
+  , ConfigPath(..)
+  , Lines
+  , Step
+  ) where
 
 --------------------------------------------------------------------------------
 import           Control.Monad                                    (foldM)
@@ -46,105 +46,105 @@ import qualified Language.Haskell.Stylish.Step.UnicodeSyntax      as UnicodeSynt
 import           Language.Haskell.Stylish.Verbose
 import           Paths_stylish_haskell                            (version)
 
-
 --------------------------------------------------------------------------------
-simpleAlign :: Maybe Int  -- ^ Columns
-            -> SimpleAlign.Config
-            -> Step
+simpleAlign ::
+     Maybe Int -- ^ Columns
+  -> SimpleAlign.Config
+  -> Step
 simpleAlign = SimpleAlign.step
 
-
 --------------------------------------------------------------------------------
-imports :: Maybe Int -- ^ columns
-        -> Imports.Options
-        -> Step
+imports ::
+     Maybe Int -- ^ columns
+  -> Imports.Options
+  -> Step
 imports = Imports.step
 
-
 --------------------------------------------------------------------------------
-languagePragmas :: Maybe Int -- ^ columns
-                -> LanguagePragmas.Style
-                -> Bool -- ^ Pad to same length in vertical mode?
-                -> Bool -- ^ remove redundant?
-                -> String -- ^ language prefix
-                -> Step
+languagePragmas ::
+     Maybe Int -- ^ columns
+  -> LanguagePragmas.Style
+  -> Bool -- ^ Pad to same length in vertical mode?
+  -> Bool -- ^ remove redundant?
+  -> String -- ^ language prefix
+  -> Step
 languagePragmas = LanguagePragmas.step
 
-
 --------------------------------------------------------------------------------
-tabs :: Int -- ^ number of spaces
-     -> Step
+tabs ::
+     Int -- ^ number of spaces
+  -> Step
 tabs = Tabs.step
-
 
 --------------------------------------------------------------------------------
 trailingWhitespace :: Step
 trailingWhitespace = TrailingWhitespace.step
 
-
 --------------------------------------------------------------------------------
-unicodeSyntax :: Bool -- ^ add language pragma?
-              -> String -- ^ language prefix
-              -> Step
+unicodeSyntax ::
+     Bool -- ^ add language pragma?
+  -> String -- ^ language prefix
+  -> Step
 unicodeSyntax = UnicodeSyntax.step
-
 
 --------------------------------------------------------------------------------
 runStep :: Extensions -> Maybe FilePath -> Lines -> Step -> Either String Lines
-runStep exts mfp ls = \case
-  Step _name step ->
-    step ls <$> parseModule exts mfp (unlines ls)
+runStep exts mfp ls =
+  \case
+    Step _name step -> step ls <$> parseModule exts mfp (unlines ls)
 
 --------------------------------------------------------------------------------
 runSteps ::
-     Extensions
-  -> Maybe FilePath
-  -> [Step]
-  -> Lines
-  -> Either String Lines
-runSteps exts mfp steps ls =
- foldM (runStep exts mfp) ls steps
+     Extensions -> Maybe FilePath -> [Step] -> Lines -> Either String Lines
+runSteps exts mfp steps ls = foldM (runStep exts mfp) ls steps
 
-newtype ConfigPath = ConfigPath { unConfigPath :: FilePath }
+newtype ConfigPath =
+  ConfigPath
+    { unConfigPath :: FilePath
+    }
 
 -- |Formats given contents optionally using the config provided as first param.
 -- The second file path is the location from which the contents were read.
 -- If provided, it's going to be printed out in the error message.
-format :: Maybe ConfigPath -> Maybe FilePath -> String -> IO (Either String Lines)
+format ::
+     Maybe ConfigPath -> Maybe FilePath -> String -> IO (Either String Lines)
 format maybeConfigPath maybeFilePath contents = do
   conf <- loadConfig (makeVerbose True) (fmap unConfigPath maybeConfigPath)
-  pure $ runSteps (configLanguageExtensions conf) maybeFilePath (configSteps conf) $ lines contents
-
+  pure $
+    runSteps (configLanguageExtensions conf) maybeFilePath (configSteps conf) $
+    lines contents
 
 --------------------------------------------------------------------------------
 -- | Searches Haskell source files in any given folder recursively.
 findHaskellFiles :: Bool -> [FilePath] -> IO [FilePath]
 findHaskellFiles v fs = mapM (findFilesR v) fs >>= return . concat
 
-
 --------------------------------------------------------------------------------
 findFilesR :: Bool -> FilePath -> IO [FilePath]
-findFilesR _ []   = return []
+findFilesR _ [] = return []
 findFilesR v path = do
   doesFileExist path >>= \case
     True -> return [path]
-    _    -> doesDirectoryExist path >>= \case
-      True  -> findFilesRecursive path >>=
-        return . filter (\x -> takeExtension x == ".hs")
-      False -> do
-        makeVerbose v ("Input folder does not exists: " <> path)
-        findFilesR v []
+    _ ->
+      doesDirectoryExist path >>= \case
+        True ->
+          findFilesRecursive path >>=
+          return . filter (\x -> takeExtension x == ".hs")
+        False -> do
+          makeVerbose v ("Input folder does not exists: " <> path)
+          findFilesR v []
   where
     findFilesRecursive :: FilePath -> IO [FilePath]
     findFilesRecursive = listDirectoryFiles findFilesRecursive
-
-    listDirectoryFiles :: (FilePath -> IO [FilePath])
-                       -> FilePath -> IO [FilePath]
+    listDirectoryFiles ::
+         (FilePath -> IO [FilePath]) -> FilePath -> IO [FilePath]
     listDirectoryFiles go topdir = do
-      ps <- listDirectory topdir >>=
-        mapM (\x -> do
-                 let dir = topdir </> x
-                 doesDirectoryExist dir >>= \case
-                   True  -> go dir
-                   False -> return [dir])
+      ps <-
+        listDirectory topdir >>=
+        mapM
+          (\x -> do
+             let dir = topdir </> x
+             doesDirectoryExist dir >>= \case
+               True  -> go dir
+               False -> return [dir])
       return $ concat ps
