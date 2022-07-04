4db9bba46ed3cbd57aa9f5af4479fa93a8202cb6
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-04 22:45:49 +0900

    Apply the formatter

--- tests/Language/Haskell/Stylish/Tests.hs
+++ tests/Language/Haskell/Stylish/Tests.hs
@@ -1,9 +1,9 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE LambdaCase #-}
-module Language.Haskell.Stylish.Tests
-    ( tests
-    ) where
 
+module Language.Haskell.Stylish.Tests
+  ( tests
+  ) where
 
 --------------------------------------------------------------------------------
 import           Data.List                           (isInfixOf, sort)
@@ -14,15 +14,15 @@ import           Test.Framework.Providers.HUnit      (testCase)
 import           Test.HUnit                          (Assertion, assertFailure,
                                                       (@?=))
 
-
 --------------------------------------------------------------------------------
 import           Language.Haskell.Stylish
 import           Language.Haskell.Stylish.Tests.Util
 
-
 --------------------------------------------------------------------------------
 tests :: Test
-tests = testGroup "Language.Haskell.Stylish.Tests"
+tests =
+  testGroup
+    "Language.Haskell.Stylish.Tests"
     [ testCase "case 01" case01
     , testCase "case 02" case02
     , testCase "case 03" case03
@@ -32,7 +32,6 @@ tests = testGroup "Language.Haskell.Stylish.Tests"
     , testCase "case 07" case07
     ]
 
-
 --------------------------------------------------------------------------------
 case01 :: Assertion
 case01 = (@?= result) =<< format Nothing Nothing input
@@ -40,11 +39,12 @@ case01 = (@?= result) =<< format Nothing Nothing input
     input = "module Herp where\ndata Foo = Bar | Baz { baz :: Int }"
     result = Right $ lines input
 
-
 --------------------------------------------------------------------------------
 case02 :: Assertion
-case02 = withTestDirTree $ do
-    writeFile "test-config.yaml" $ unlines
+case02 =
+  withTestDirTree $ do
+    writeFile "test-config.yaml" $
+      unlines
         [ "steps:"
         , "  - records:"
         , "      equals: \"indent 2\""
@@ -53,23 +53,26 @@ case02 = withTestDirTree $ do
         , "      deriving: 2"
         , "      via: \"indent 2\""
         ]
-
     actual <- format (Just $ ConfigPath "test-config.yaml") Nothing input
     actual @?= result
   where
     input = "module Herp where\ndata Foo = Bar | Baz { baz :: Int }"
-    result = Right [ "module Herp where"
-                   , "data Foo"
-                   , "  = Bar"
-                   , "  | Baz"
-                   , "      { baz :: Int"
-                   , "      }"
-                   ]
+    result =
+      Right
+        [ "module Herp where"
+        , "data Foo"
+        , "  = Bar"
+        , "  | Baz"
+        , "      { baz :: Int"
+        , "      }"
+        ]
 
 --------------------------------------------------------------------------------
 case03 :: Assertion
-case03 = withTestDirTree $ do
-    writeFile "test-config.yaml" $ unlines
+case03 =
+  withTestDirTree $ do
+    writeFile "test-config.yaml" $
+      unlines
         [ "steps:"
         , "  - records:"
         , "      equals: \"same_line\""
@@ -78,75 +81,77 @@ case03 = withTestDirTree $ do
         , "      deriving: 2"
         , "      via: \"indent 2\""
         ]
-
     actual <- format (Just $ ConfigPath "test-config.yaml") Nothing input
     actual @?= result
   where
-    input = unlines [ "module Herp where"
-                    , "data Foo"
-                    , "  = Bar"
-                    , "  | Baz"
-                    , "      { baz :: Int"
-                    , "      }"
-                    ]
-    result = Right [ "module Herp where"
-                   , "data Foo = Bar"
-                   , "         | Baz { baz :: Int"
-                   , "               }"
-                   ]
-
+    input =
+      unlines
+        [ "module Herp where"
+        , "data Foo"
+        , "  = Bar"
+        , "  | Baz"
+        , "      { baz :: Int"
+        , "      }"
+        ]
+    result =
+      Right
+        [ "module Herp where"
+        , "data Foo = Bar"
+        , "         | Baz { baz :: Int"
+        , "               }"
+        ]
 
 --------------------------------------------------------------------------------
 case04 :: Assertion
-case04 = format Nothing (Just fileLocation) input >>= \case
-    Right _                      -> assertFailure "expected error"
+case04 =
+  format Nothing (Just fileLocation) input >>= \case
+    Right _ -> assertFailure "expected error"
     Left err
-        | fileLocation `isInfixOf` err
-        , needle `isInfixOf` err -> pure ()
-        | otherwise              ->
-            assertFailure $ "Unexpected error: " ++ show err
+      | fileLocation `isInfixOf` err
+      , needle `isInfixOf` err -> pure ()
+      | otherwise -> assertFailure $ "Unexpected error: " ++ show err
   where
     input = "module Herp"
     fileLocation = "directory/File.hs"
     needle = "possibly incorrect indentation or mismatched brackets"
 
-
 --------------------------------------------------------------------------------
 -- | When providing current dir including folders and files.
 case05 :: Assertion
-case05 = withTestDirTree $ do
-  createDirectory aDir >> writeFile c fileCont
-  mapM_ (flip writeFile fileCont) fs
-  result <- findHaskellFiles False input
-  sort result @?= (sort $ map normalise expected)
+case05 =
+  withTestDirTree $ do
+    createDirectory aDir >> writeFile c fileCont
+    mapM_ (flip writeFile fileCont) fs
+    result <- findHaskellFiles False input
+    sort result @?= (sort $ map normalise expected)
   where
-    input    = c : fs
+    input = c : fs
     fs = ["b.hs", "a.hs"]
-    c  = aDir </> "c.hs"
-    aDir     = "aDir"
+    c = aDir </> "c.hs"
+    aDir = "aDir"
     expected = ["a.hs", "b.hs", c]
     fileCont = ""
-
 
 --------------------------------------------------------------------------------
 -- | When the input item is not file, do not recurse it.
 case06 :: Assertion
-case06 = withTestDirTree $ do
-  mapM_ (flip writeFile "") input
-  result <- findHaskellFiles False input
-  result @?= expected
+case06 =
+  withTestDirTree $ do
+    mapM_ (flip writeFile "") input
+    result <- findHaskellFiles False input
+    result @?= expected
   where
-    input    = ["b.hs"]
+    input = ["b.hs"]
     expected = map normalise input
-
 
 --------------------------------------------------------------------------------
 -- | Empty input should result in empty output.
 case07 :: Assertion
-case07 = withTestDirTree $ do
-  mapM_ (flip writeFile "") input
-  result <- findHaskellFiles False input
-  result @?= expected
+case07 =
+  withTestDirTree $ do
+    mapM_ (flip writeFile "") input
+    result <- findHaskellFiles False input
+    result @?= expected
   where
-    input    = []
+    input = []
     expected = input
