7e16aa362b433f393c9deb6bcd077a9dbe2bd639
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-04 09:14:18 +0900

    Remove unused functions

--- lib/Language/Haskell/Stylish/GHC.hs
+++ lib/Language/Haskell/Stylish/GHC.hs
@@ -4,15 +4,7 @@
 
 -- | Utility functions for working with the GHC AST
 module Language.Haskell.Stylish.GHC
-  ( dropAfterLocated
-  , dropBeforeLocated
-  , dropBeforeAndAfter
-    -- * Unsafe getters
-  , unsafeGetRealSrcSpan
-  , getEndLineUnsafe
-  , getStartLineUnsafe
-    -- * Standard settings
-  , baseDynFlags
+  ( baseDynFlags
     -- * Outputable operators
   , showOutputable
     -- * Deconstruction
@@ -40,36 +32,6 @@ import           GHC.Types.SrcLoc                                    (GenLocated
 import qualified GHC.Types.SrcLoc                                    as GHC
 import qualified GHC.Utils.Outputable                                as GHC
 import qualified Language.Haskell.GhclibParserEx.GHC.Settings.Config as GHCEx
-
-unsafeGetRealSrcSpan :: Located a -> RealSrcSpan
-unsafeGetRealSrcSpan =
-  \case
-    (L (RealSrcSpan s _) _) -> s
-    _                       -> error "could not get source code location"
-
-getStartLineUnsafe :: Located a -> Int
-getStartLineUnsafe = srcSpanStartLine . unsafeGetRealSrcSpan
-
-getEndLineUnsafe :: Located a -> Int
-getEndLineUnsafe = srcSpanEndLine . unsafeGetRealSrcSpan
-
-dropAfterLocated :: Maybe (Located a) -> [RealLocated b] -> [RealLocated b]
-dropAfterLocated loc xs =
-  case loc of
-    Just (L (RealSrcSpan rloc _) _) ->
-      filter (\(L x _) -> srcSpanEndLine rloc >= srcSpanStartLine x) xs
-    _ -> xs
-
-dropBeforeLocated :: Maybe (Located a) -> [RealLocated b] -> [RealLocated b]
-dropBeforeLocated loc xs =
-  case loc of
-    Just (L (RealSrcSpan rloc _) _) ->
-      filter (\(L x _) -> srcSpanStartLine rloc <= srcSpanEndLine x) xs
-    _ -> xs
-
-dropBeforeAndAfter :: Located a -> [RealLocated b] -> [RealLocated b]
-dropBeforeAndAfter loc =
-  dropBeforeLocated (Just loc) . dropAfterLocated (Just loc)
 
 baseDynFlags :: GHC.DynFlags
 baseDynFlags = defaultDynFlags GHCEx.fakeSettings GHCEx.fakeLlvmConfig
