2d4dd3acb5231a64a916f16afd1d97585dce9a31
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-04 23:52:41 +0900

    Apply the formatter

--- lib/Language/Haskell/Stylish/Config/Cabal.hs
+++ lib/Language/Haskell/Stylish/Config/Cabal.hs
@@ -1,8 +1,7 @@
 --------------------------------------------------------------------------------
 module Language.Haskell.Stylish.Config.Cabal
-    ( findLanguageExtensions
-    ) where
-
+  ( findLanguageExtensions
+  ) where
 
 --------------------------------------------------------------------------------
 import           Data.Either                              (isRight)
@@ -16,76 +15,78 @@ import qualified Language.Haskell.Extension               as Language
 import           Language.Haskell.Stylish.Verbose
 import           System.Directory                         (getCurrentDirectory)
 
-
 --------------------------------------------------------------------------------
 import           Language.Haskell.Stylish.Config.Internal
-
 
 --------------------------------------------------------------------------------
 findLanguageExtensions :: Verbose -> IO [Language.KnownExtension]
 findLanguageExtensions verbose =
-    findCabalFile verbose >>=
-    maybe (pure []) (readDefaultLanguageExtensions verbose)
-
+  findCabalFile verbose >>=
+  maybe (pure []) (readDefaultLanguageExtensions verbose)
 
 --------------------------------------------------------------------------------
 -- | Find the closest .cabal file, possibly going up the directory structure.
 findCabalFile :: Verbose -> IO (Maybe FilePath)
 findCabalFile verbose = do
   potentialProjectRoots <- ancestors <$> getCurrentDirectory
-  potentialCabalFile <- filter isRight <$>
-    traverse Cabal.findPackageDesc potentialProjectRoots
+  potentialCabalFile <-
+    filter isRight <$> traverse Cabal.findPackageDesc potentialProjectRoots
   case potentialCabalFile of
     [Right cabalFile] -> return (Just cabalFile)
     _ -> do
-      verbose $ ".cabal file not found, directories searched: " <>
+      verbose $
+        ".cabal file not found, directories searched: " <>
         show potentialProjectRoots
-      verbose $ "Stylish Haskell will work basing on LANGUAGE pragmas in source files."
+      verbose $
+        "Stylish Haskell will work basing on LANGUAGE pragmas in source files."
       return Nothing
-
 
 --------------------------------------------------------------------------------
 -- | Extract @default-extensions@ fields from a @.cabal@ file
-readDefaultLanguageExtensions :: Verbose -> FilePath -> IO [Language.KnownExtension]
+readDefaultLanguageExtensions ::
+     Verbose -> FilePath -> IO [Language.KnownExtension]
 readDefaultLanguageExtensions verbose cabalFile = do
   verbose $ "Parsing " <> cabalFile <> "..."
-  packageDescription <- Cabal.readGenericPackageDescription Cabal.silent cabalFile
+  packageDescription <-
+    Cabal.readGenericPackageDescription Cabal.silent cabalFile
   let library :: [Cabal.Library]
-      library = maybeToList $ fst . Cabal.ignoreConditions <$>
-        Cabal.condLibrary packageDescription
-
+      library =
+        maybeToList $
+        fst . Cabal.ignoreConditions <$> Cabal.condLibrary packageDescription
       subLibraries :: [Cabal.Library]
-      subLibraries = fst . Cabal.ignoreConditions . snd <$>
+      subLibraries =
+        fst . Cabal.ignoreConditions . snd <$>
         Cabal.condSubLibraries packageDescription
-
       executables :: [Cabal.Executable]
-      executables = fst . Cabal.ignoreConditions . snd <$>
+      executables =
+        fst . Cabal.ignoreConditions . snd <$>
         Cabal.condExecutables packageDescription
-
       testSuites :: [Cabal.TestSuite]
-      testSuites = fst . Cabal.ignoreConditions . snd <$>
+      testSuites =
+        fst . Cabal.ignoreConditions . snd <$>
         Cabal.condTestSuites packageDescription
-
       benchmarks :: [Cabal.Benchmark]
-      benchmarks = fst . Cabal.ignoreConditions . snd <$>
+      benchmarks =
+        fst . Cabal.ignoreConditions . snd <$>
         Cabal.condBenchmarks packageDescription
-
       gatherBuildInfos :: [Cabal.BuildInfo]
-      gatherBuildInfos = map Cabal.libBuildInfo library <>
-                         map Cabal.libBuildInfo subLibraries <>
-                         map Cabal.buildInfo executables <>
-                         map Cabal.testBuildInfo testSuites <>
-                         map Cabal.benchmarkBuildInfo benchmarks
-
+      gatherBuildInfos =
+        map Cabal.libBuildInfo library <>
+        map Cabal.libBuildInfo subLibraries <>
+        map Cabal.buildInfo executables <>
+        map Cabal.testBuildInfo testSuites <>
+        map Cabal.benchmarkBuildInfo benchmarks
       defaultExtensions :: [Language.KnownExtension]
-      defaultExtensions = map fromEnabled . filter isEnabled $
+      defaultExtensions =
+        map fromEnabled . filter isEnabled $
         concatMap Cabal.defaultExtensions gatherBuildInfos
-        where isEnabled (Language.EnableExtension _) = True
-              isEnabled _                            = False
-
-              fromEnabled (Language.EnableExtension x) = x
-              fromEnabled x                             =
-                error $ "Language.Haskell.Stylish.Config.readLanguageExtensions: " <>
-                        "invalid LANGUAGE pragma:  " <> show x
+        where
+          isEnabled (Language.EnableExtension _) = True
+          isEnabled _                            = False
+          fromEnabled (Language.EnableExtension x) = x
+          fromEnabled x =
+            error $
+            "Language.Haskell.Stylish.Config.readLanguageExtensions: " <>
+            "invalid LANGUAGE pragma:  " <> show x
   verbose $ "Gathered default-extensions: " <> show defaultExtensions
   pure $ nub defaultExtensions
