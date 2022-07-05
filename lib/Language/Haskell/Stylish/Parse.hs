9e6a85dfe0b9340054f10762417cbdd48ac6e409
Hiroki Tokunaga <tokusan441@gmail.com>
2022-07-05 09:10:54 +0900

    Apply the formatter

--- lib/Language/Haskell/Stylish/Parse.hs
+++ lib/Language/Haskell/Stylish/Parse.hs
@@ -3,7 +3,6 @@ module Language.Haskell.Stylish.Parse
   ( parseModule
   ) where
 
-
 --------------------------------------------------------------------------------
 import           Control.Monad                                      ((>=>))
 import           Data.List                                          (foldl',
@@ -25,74 +24,79 @@ import qualified GHC.Utils.Outputable                               as GHC
 import qualified Language.Haskell.GhclibParserEx.GHC.Driver.Session as GHCEx
 import qualified Language.Haskell.GhclibParserEx.GHC.Parser         as GHCEx
 
-
 --------------------------------------------------------------------------------
 import           Language.Haskell.Stylish.GHC
 import           Language.Haskell.Stylish.Module
 
-
 --------------------------------------------------------------------------------
 type Extensions = [String]
-
 
 --------------------------------------------------------------------------------
 -- | Filter out lines which use CPP macros
 unCpp :: String -> String
 unCpp = unlines . go False . lines
   where
-    go _           []       = []
-    go isMultiline (x : xs) =
-        let isCpp         = isMultiline || listToMaybe x == Just '#'
-            nextMultiline = isCpp && not (null x) && last x == '\\'
-        in (if isCpp then "" else x) : go nextMultiline xs
-
+    go _ [] = []
+    go isMultiline (x:xs) =
+      let isCpp = isMultiline || listToMaybe x == Just '#'
+          nextMultiline = isCpp && not (null x) && last x == '\\'
+       in (if isCpp
+             then ""
+             else x) :
+          go nextMultiline xs
 
 --------------------------------------------------------------------------------
 -- | If the given string is prefixed with an UTF-8 Byte Order Mark, drop it
 -- because haskell-src-exts can't handle it.
 dropBom :: String -> String
-dropBom ('\xfeff' : str) = str
-dropBom str              = str
-
+dropBom ('\xfeff':str) = str
+dropBom str            = str
 
 --------------------------------------------------------------------------------
 -- | Abstraction over GHC lib's parsing
 parseModule :: Extensions -> Maybe FilePath -> String -> Either String Module
-parseModule externalExts0 fp string = do
+parseModule externalExts0 fp string
     -- Parse extensions.
-    externalExts1 <- for externalExts0 $ \s -> case GHCEx.readExtension s of
+ = do
+  externalExts1 <-
+    for externalExts0 $ \s ->
+      case GHCEx.readExtension s of
         Nothing -> Left $ "Unknown extension: " ++ show s
         Just e  -> Right e
-
     -- Build first dynflags.
-    let dynFlags0 = foldl' turnOn baseDynFlags externalExts1
-
+  let dynFlags0 = foldl' turnOn baseDynFlags externalExts1
     -- Parse options from file
-    let fileOptions = fmap GHC.unLoc $ GHC.getOptions dynFlags0
-            (GHC.stringToStringBuffer string)
-            (fromMaybe "-" fp)
-        fileExtensions = mapMaybe
-            (stripPrefix "-X" >=> GHCEx.readExtension)
-            fileOptions
-
+  let fileOptions =
+        GHC.unLoc <$>
+        GHC.getOptions
+          dynFlags0
+          (GHC.stringToStringBuffer string)
+          (fromMaybe "-" fp)
+      fileExtensions =
+        mapMaybe (stripPrefix "-X" >=> GHCEx.readExtension) fileOptions
     -- Set further dynflags.
-    let dynFlags1 = foldl' turnOn dynFlags0 fileExtensions
-            `GHC.gopt_set` GHC.Opt_KeepRawTokenStream
-
+  let dynFlags1 =
+        foldl' turnOn dynFlags0 fileExtensions `GHC.gopt_set`
+        GHC.Opt_KeepRawTokenStream
     -- Possibly strip CPP.
-    let removeCpp s = if GHC.xopt LangExt.Cpp dynFlags1 then unCpp s else s
-        input = removeCpp $ dropBom string
-
+  let removeCpp s =
+        if GHC.xopt LangExt.Cpp dynFlags1
+          then unCpp s
+          else s
+      input = removeCpp $ dropBom string
     -- Actual parse.
-    case GHCEx.parseModule input dynFlags1 of
-        GHC.POk _ m -> Right m
-        GHC.PFailed ps -> Left . withFileName . GHC.showSDoc dynFlags1 .
-            GHC.vcat . GHC.pprMsgEnvelopeBagWithLoc . fmap GHC.pprError . snd $
-            GHC.getMessages ps
+  case GHCEx.parseModule input dynFlags1 of
+    GHC.POk _ m -> Right m
+    GHC.PFailed ps ->
+      Left .
+      withFileName .
+      GHC.showSDoc dynFlags1 .
+      GHC.vcat . GHC.pprMsgEnvelopeBagWithLoc . fmap GHC.pprError . snd $
+      GHC.getMessages ps
   where
     withFileName x = maybe "" (<> ": ") fp <> x
-
-    turnOn dynFlags ext = foldl'
+    turnOn dynFlags ext =
+      foldl'
         turnOn
         (GHC.xopt_set dynFlags ext)
         [rhs | (lhs, True, rhs) <- GHC.impliedXFlags, lhs == ext]
